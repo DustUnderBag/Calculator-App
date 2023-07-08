052918b4e6dc2b2c5f966026ed419a8a3210d573
DustUnderBag <61408175+DustUnderBag@users.noreply.github.com>
2023-07-08 23:28:30 +0800

    Rewrites code, initializes variables again

--- script.js
+++ script.js
@@ -1,58 +1,20 @@
 const display = document.querySelector('div#display');
+const buttons = document.querySelectorAll('button');
 const digits = document.querySelectorAll('button.digit');
 const operators = document.querySelectorAll('button.operator');
+const equal = document.querySelector('button.equal');
 
-console.log(digits);
-console.log(operators);
+let a = 0;
+let b = 0;
+let op = "";
 
-let firstDigit = 5;
-let secondDigit = 13;
-let currentOperator = "+";
-
-let displayContent = firstDigit + currentOperator + secondDigit;
-
-display.textContent = displayContent;
-
-digits.forEach(digit => 
-    digit.addEventListener('click', e => {
-    console.log(e.target.id);
-    })
-);
-
-operators.forEach(operator => 
-    operator.addEventListener('click', e => {
-    console.log(e.target.id);
-    })
-);
-
-
-function add(a, b) {
-    return a + b;
-}
-
-function substract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(a, b, operator) {
-    let answer = 
-        (operator == "+")
-        ? add(a,b)
-        :(operator == "-")
-        ? substract(a,b)
-        :(operator == "*")
-        ? multiply(a,b)
-        : divide(a,b);
-
-    return answer;
-}
-
-console.log(operate(firstDigit, secondDigit, currentOperator));
+const methods = {
+    "+": function(a, b) {
+        return a + b;
+    },
+    "-"(a, b) {
+        return a -b;
+    },
+    "*": (a, b) => a * b,
+    "/": (a, b) => a / b,
+};
